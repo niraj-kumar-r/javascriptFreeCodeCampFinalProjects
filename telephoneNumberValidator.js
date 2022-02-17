498c65c5fb577f0081e7feb612d575eaf256b71f
niraj-kumar-r <nirajk23523@gmail.com>
2022-02-17 18:34:02 +0530

    dealt with the country code being only 1

--- telephoneNumberValidator.js
+++ telephoneNumberValidator.js
@@ -12,7 +12,11 @@
 // For this challenge you will be presented with a string such as 800-692-7753 or 8oo-six427676;laskdjf. Your job is to validate or reject the US phone number based on any combination of the formats provided above. The area code is required. If the country code is provided, you must confirm that the country code is 1. Return true if the string is a valid US phone number; otherwise return false.
 
 function telephoneCheck(str) {
-  return true;
+    if (/^\d\s|^\d\(/.test(str)) {
+        if (str[0] !== "1") {
+            return false;
+        }
+    }
 }
 
 console.log(telephoneCheck("555-555-5555"));
