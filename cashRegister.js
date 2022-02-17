dc904c60afca39ce9bbe7b2a1172353f0bfc6f0c
niraj-kumar-r <nirajk23523@gmail.com>
2022-02-17 20:40:03 +0530

    added the valueArr that holds the value of different notes

--- cashRegister.js
+++ cashRegister.js
@@ -36,18 +36,31 @@
 // ]
 
 function checkCashRegister(price, cash, cid) {
-    let change;
+    let change = cash - price;
+    let valueArr = {
+        PENNY: 0.01,
+        NICKEL: 0.05,
+        DIME: 0.1,
+        DOLLAR: 1,
+        QUARTER: 0.25,
+        FIVE: 5,
+        TEN: 10,
+        TWENTY: 20,
+        "ONE HUNDRED": 100,
+    };
     return change;
 }
 
-checkCashRegister(19.5, 20, [
-    ["PENNY", 1.01],
-    ["NICKEL", 2.05],
-    ["DIME", 3.1],
-    ["QUARTER", 4.25],
-    ["ONE", 90],
-    ["FIVE", 55],
-    ["TEN", 20],
-    ["TWENTY", 60],
-    ["ONE HUNDRED", 100],
-]);
+console.log(
+    checkCashRegister(19.5, 20, [
+        ["PENNY", 1.01],
+        ["NICKEL", 2.05],
+        ["DIME", 3.1],
+        ["QUARTER", 4.25],
+        ["ONE", 90],
+        ["FIVE", 55],
+        ["TEN", 20],
+        ["TWENTY", 60],
+        ["ONE HUNDRED", 100],
+    ])
+);
